87552891e05bb222316543352383e9193bc62c64
austinxduong <axduong2@gmail.com>
2021-03-23 20:41:15 -0700

    got button to work

--- app.js
+++ app.js
@@ -3,3 +3,25 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const button = document.getElementById('button');
+const testResults = document.getElementById('test-results');
+
+let score = 0
+
+button.addEventListener('click' , () => {
+
+    alert('welcome to my quiz');
+    const confirmQuiz = confirm('Quizz time');
+    if(!confirmQuiz){
+        alert('thank you')
+        return;
+    }
+const userInput = prompt ('Q');
+const firstAnswer = isYes(userInput);
+if(firstAnswer === true);
+    score ++
+
+testResults.textContent = score;
+
+});
